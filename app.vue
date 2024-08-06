0ea336f2c33989bdade39e3efe1f259127fe1bd2
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2024-08-05 22:29:30 -0300

    :tada: init

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold text-red-600 underline bg-red-100">
+      Hello world!
+    </h1>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
